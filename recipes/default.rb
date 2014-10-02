e9f54bbf8302ebb58406fa569661d2f2d0d03d0e
Andrii Starodubets <astarodubets@gmail.com>
2014-10-02 12:06:22 +0300

    Added update main config module

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,2 @@
 include_recipe 'chef-openswan::install'
-
-file = Chef::Util::FileEdit.new('/etc/ipsec.conf')
-file.search_file_replace_line('^#include /etc/ipsec\.d/\*\.conf$', "include /etc/ipsec.d/*.conf")
-file.write_file
+include_recipe 'chef-openswan::updatemainconf'
---
+++ recipes/updatemainconf.rb
@@ -0,0 +1,3 @@
+file = Chef::Util::FileEdit.new('/etc/ipsec.conf')
+file.search_file_replace_line('^#include /etc/ipsec\.d/\*\.conf$', "include /etc/ipsec.d/*.conf")
+file.write_file
