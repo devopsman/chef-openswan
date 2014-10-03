b11d297e37a2f1fcd094f0afe010e4869df112e4
Andrii Starodubets <astarodubets@gmail.com>
2014-10-03 17:17:28 +0300

    Added fixes

--- recipes/createvpnconf.rb
+++ recipes/createvpnconf.rb
@@ -1,5 +1,13 @@
+### IMPORTANT - make sure there is a newline in the end of vpc-to-vpc.conf file. Otherwise ipsec fails to start with Segmentation fault
 template "/etc/ipsec.d/vpc-to-vpc.conf" do
   source "vpc-to-vpc.erb"
+  mode '0440'
+  owner 'root'
+  group 'root'
+end
+
+template "/etc/ipsec.d/vpc-to-vpc.secrets" do
+  source "vpc-to-vpc-secrets.erb"
   mode '0440'
   owner 'root'
   group 'root'
--- recipes/updatesysctl.rb
+++ recipes/updatesysctl.rb
@@ -3,6 +3,8 @@ file = Chef::Util::FileEdit.new('/etc/sysctl.conf')
 file.insert_line_if_no_match('^net.ipv4.ip_forward = 1$', "net.ipv4.ip_forward = 1")
 file.insert_line_if_no_match('^net.ipv4.conf.all.accept_redirects = 0$', "net.ipv4.conf.all.accept_redirects = 0")
 file.insert_line_if_no_match('^net.ipv4.conf.all.send_redirects = 0$', "net.ipv4.conf.all.send_redirects = 0")
+file.insert_line_if_no_match('^net.ipv4.conf.default.accept_redirects = 0$', "net.ipv4.conf.default.accept_redirects = 0")
+file.insert_line_if_no_match('^net.ipv4.conf.default.send_redirects = 0$', "net.ipv4.conf.default.send_redirects = 0")
 file.write_file
 
 execute "" do
